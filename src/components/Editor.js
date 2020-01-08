33fae5b24310b276b387f898c170050977479f67
haesooKr <gotn676@gmail.com>
2020-01-08 00:47:45 -0500

    Added codemirror on Editor.js

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,7 +1,33 @@
 import React, { Component } from 'react'
 import './Editor.css';
 
+import CodeMirror from 'codemirror';
+import 'codemirror/mode/markdown/markdown';
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/mode/jsx/jsx';
+import 'codemirror/mode/css/css';
+import 'codemirror/mode/shell/shell';
+
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/monokai.css';
+
 export default class Editor extends Component {
+  editor = null;
+  codeMirror = null;
+
+  initializeEditor = () => {
+    this.codeMirror = CodeMirror(this.editor, {
+      mode: 'markdown',
+      theme: 'monokai',
+      lineNumbers: true,
+      lineWrapping: true
+    });
+  }
+
+  componentDidMount(){
+    this.initializeEditor();
+  }
+
   render() {
     return (
       <div className="editor">
@@ -10,8 +36,8 @@ export default class Editor extends Component {
           placeholder="Enter the title"
           name="title"
         />
-        <div className="markdown">
-          Markdown
+        <div className="markdown" ref={ref => this.editor=ref}>
+          {/* markdown */}
         </div>
       </div>
     )
