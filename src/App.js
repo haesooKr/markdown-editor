f45d5790567bdbc1648739f05e9c0609e1f8daa3
haesooKr <gotn676@gmail.com>
2020-01-08 00:28:39 -0500

    Added Preview Panel

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Editor from './components/Editor';
+import Preview from './components/Preview';
 
 function App() {
   return (
     <div className="App">
       <Editor/>
+      <Preview/>
     </div>
   );
 }
---
+++ src/components/Preview.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Preview() {
+  return (
+    <div>
+      <div className="preview-title">
+        Title
+      </div>
+      <div className="preview-markdown">
+        Markdown
+      </div>
+    </div>
+  )
+}
