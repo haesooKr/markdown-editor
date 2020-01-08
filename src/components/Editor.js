6a7c6cd1997be89686dbe9343efa79ca34746bf7
haesooKr <gotn676@gmail.com>
2020-01-08 00:31:43 -0500

    Added className

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -3,13 +3,13 @@ import React, { Component } from 'react'
 export default class Editor extends Component {
   render() {
     return (
-      <div>
+      <div className="editor">
         <input
           className="title"
           placeholder="Enter the title"
           name="title"
         />
-        <div>
+        <div className="markdown">
           Markdown
         </div>
       </div>
