9f54a28b585b433cfa8654577d626a499f2056c7
haesooKr <gotn676@gmail.com>
2020-01-08 19:51:51 -0500

    Added Preview Button Function

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,17 @@ import React from 'react'
 import './Header.css';
 
 export default function Header() {
+  const preview = document.getElementsByClassName('preview');
+  const editor = document.getElementsByClassName('editor');
+  console.log(preview, editor);
+  const handlePreviewClick = () => {
+    preview[0].classList.toggle("show");
+    editor[0].classList.toggle("hide")
+  }
   return (
       <header>
         <div className="title">Markdown Editor</div>
-        <button className="header__preview">Preview</button>
+        <button className="header__preview" onClick={handlePreviewClick}>Preview</button>
         <button className="header__save">Save </button>
       </header>
   )
